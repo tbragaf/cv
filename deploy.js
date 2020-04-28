199ee6d900054394821d2b8a3562bc74adebc74d
Tiago Braga <braga.tiago.93@gmail.com>
2020-04-28 08:25:53 +0100

    Delete everything

--- deploy.js
+++
@@ -1,5 +0,0 @@
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', {
-    branch: 'master'
-}, function(err) { });
--- src/app/components/components.ts
+++
@@ -1,7 +0,0 @@
-export { Header } from './header/header';
-export { Welcome } from './welcome/welcome';
-export { About } from './about/about';
-export { Hobbies } from './hobbies/hobbies';
-export { Skills } from './skills/skills';
-export { Timeline } from './timeline/timeline';
-export { Contact } from './contact/contact';
